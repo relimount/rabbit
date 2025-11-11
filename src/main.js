72fbb2bf663219b726a5558254132d4fec9b1765
胡唐峰 <152974699+relimount@users.noreply.github.com>
2025-11-11 19:11:52 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import './assets/main.css'
-
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import '@/styles/common.scss'
 
 const app = createApp(App)
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,23 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
+
+import { useCategoryStore } from '@/stores/category'
+import { onMounted,ref } from 'vue'
+
+const categoryStore = useCategoryStore()
+
+  onMounted(()=>{
+    categoryStore.getCategory()
+  })
+</script>
+
 <template>
-  <div>我是首页</div>
+  <LayoutFixed/>
+  <LayoutNav />
+  <LayoutHeader />
   <RouterView />
+  <LayoutFooter />
 </template>
