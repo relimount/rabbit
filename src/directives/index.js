7832a3554dd104bc3395550733ddcd5efd2fe780
胡唐峰 <152974699+relimount@users.noreply.github.com>
2025-11-12 09:57:06 +0800

    懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,21 @@
+import { useIntersectionObserver } from '@vueuse/core'
+import { install } from 'element-plus'
+
+export const lazyPlugin = {
+  install(app){
+    app.directive('img-lazy', {
+    mounted(el,binding){
+      const { stop } = useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+          if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+          stop()
+          }
+        },
+      )
+    }
+})
+  }
+}
