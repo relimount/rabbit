27a96c3531c12f9cfb01b952585e814883940f1b
胡唐峰 <152974699+relimount@users.noreply.github.com>
2025-12-08 15:27:32 +0800

    图片预览组件-小图切换大图显示

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 // 获取商品信息
 const goods = ref({})
@@ -38,7 +39,7 @@ onMounted(()=>{
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
