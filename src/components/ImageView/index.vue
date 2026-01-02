75b0c37f1c7dd0635421ceafdbab66094b9a3697
胡唐峰 <152974699+relimount@users.noreply.github.com>
2026-01-02 11:26:39 +0800

    鼠标跟随滑块移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -10,10 +11,34 @@ const imageList = [
 ]
 
 const activeIndex = ref(0)
-
+//小图列表索引
 const changeIndex = (i)=>{
   activeIndex.value = i
 }
+
+const target = ref(null)
+const left = ref(0)
+const top = ref(0)
+
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+watch([elementX, elementY],()=>{
+  if(elementX.value > 100 && elementX.value < 300){
+    left.value = elementX.value - 100
+  }else if(elementX.value <= 100){
+    left.value = 0
+  }else if(elementX.value >= 300){
+    left.value = 200
+  }
+  if(elementY.value > 100 && elementY.value < 300){
+    top.value = elementY.value - 100
+  }else if(elementY.value <= 100){
+    top.value = 0
+  }else if(elementY.value >= 300){
+    top.value = 200
+  }
+})
+
 </script>
 
 
@@ -23,7 +48,7 @@ const changeIndex = (i)=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
